7ce50f0ec79827fc1f2859443567d00e40dc1de9
James Cottrill <james@slatedata.co.uk>
2026-09-13 14:54:27 +0100

    chore(deps): remove MUI and Emotion, no longer used

--- src/renderer/index.tsx
+++ src/renderer/index.tsx
@@ -1,10 +1,4 @@
 import { createRoot } from 'react-dom/client';
-import { CssBaseline } from '@mui/material';
-import {
-  createTheme,
-  StyledEngineProvider,
-  ThemeProvider,
-} from '@mui/material/styles';
 import React from 'react';
 import App from './App';
 // import ReactDOM from 'react-dom';
@@ -14,29 +8,9 @@ import './App.css';
 const rootElement = document.getElementById('root') as HTMLElement;
 const root = createRoot(rootElement);
 
-const theme = createTheme({
-  components: {
-    MuiPopover: {
-      defaultProps: {
-        container: rootElement,
-      },
-    },
-    MuiPopper: {
-      defaultProps: {
-        container: rootElement,
-      },
-    },
-  },
-});
-
 root.render(
   <React.StrictMode>
-    <StyledEngineProvider injectFirst>
-      <ThemeProvider theme={theme}>
-        <CssBaseline />
-        <App />
-      </ThemeProvider>
-    </StyledEngineProvider>
+    <App />
   </React.StrictMode>,
 );
 
